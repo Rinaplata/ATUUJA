aa932d0a0f68829095c242a90d5d8910d578c159
rina plata <rinamarcelaplata@gmail.com>
2024-09-24 23:21:49 -0500

    feat: Modal de eliminar usuarios

--- atuuja-web/src/components/Main/User/UserTable.tsx
+++ atuuja-web/src/components/Main/User/UserTable.tsx
@@ -1,12 +1,41 @@
 import React, { useState } from 'react';
-import { PencilIcon, ChartBarIcon, TrashIcon } from '@heroicons/react/24/outline';
+import {
+  PencilIcon,
+  ChartBarIcon,
+  TrashIcon,
+} from '@heroicons/react/24/outline';
 import { User } from '../../../types/user';
+import Modal from '../../Tables/Modal';
 
 interface IUserTable {
-    users: User[];
+  users: User[];
 }
 
-const TableThree : React.FC<IUserTable> = ({ users }) => {
+const TableThree: React.FC<IUserTable> = ({ users }) => {
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [modalTitle, setModalTitle] = useState('');
+    const [selectedUser, setSelectedUser] = useState<User | null>(null);
+    const [isDeleteAction, setDeleteAction] = useState(false);
+  
+    const handleOpenModal = (title: string, user: User, deleteAction = false) => {
+      setModalTitle(title);
+      setSelectedUser(user);
+      setDeleteAction(deleteAction);
+      setModalOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setModalOpen(false);
+      setSelectedUser(null);
+      setDeleteAction(false);
+    };
+  
+    const handleDeleteUser = () => {
+      // Aquí va la lógica para eliminar al usuario
+      console.log(`Eliminando al usuario: ${selectedUser?.Username}`);
+      handleCloseModal(); // Cerrar el modal después de eliminar
+    };
+
   return (
     <div className="rounded-sm bg-transparent px-5 pt-6 pb-2.5">
       <div className="max-w-full overflow-x-auto">
@@ -36,14 +65,10 @@ const TableThree : React.FC<IUserTable> = ({ users }) => {
                   </h5>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                  <p className="text-black dark:text-white">
-                    {user.Email}
-                  </p>
+                  <p className="text-black dark:text-white">{user.Email}</p>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                  <p className="text-black dark:text-white">
-                    {user.Username}
-                  </p>
+                  <p className="text-black dark:text-white">{user.Username}</p>
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
                   <div className="flex items-center space-x-3.5">
@@ -53,7 +78,7 @@ const TableThree : React.FC<IUserTable> = ({ users }) => {
                     <button className="hover:text-primaryAtuuja">
                       <ChartBarIcon className="h-5 w-5" />
                     </button>
-                    <button className="hover:text-primaryAtuuja">
+                    <button className="hover:text-primaryAtuuja" onClick={() =>handleOpenModal(`Eliminar Usuario: ${user.Username}`, user, true)}>
                       <TrashIcon className="h-5 w-5" />
                     </button>
                   </div>
@@ -63,7 +88,28 @@ const TableThree : React.FC<IUserTable> = ({ users }) => {
           </tbody>
         </table>
       </div>
-    </div>
+     {/* Modal */}
+     <Modal isOpen={isModalOpen} onClose={handleCloseModal} title={modalTitle}>
+        {isDeleteAction ? (
+          <div>
+            <p>¿Estás seguro que deseas eliminar este usuario?</p>
+            <div className="flex justify-end space-x-4 mt-4">
+              <button onClick={handleCloseModal} className="bg-gray-200 text-black px-4 py-2 rounded-lg hover:bg-gray-300">
+                Cancelar
+              </button>
+              <button onClick={handleDeleteUser} className="bg-primaryAtuuja text-white px-4 py-2 rounded-lg hover:bg-primaryAtuuja-700">
+                Sí, estoy seguro
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p>Acción para el usuario: {selectedUser?.Username}</p>
+            {/* Aquí puedes agregar más contenido para editar o ver detalles */}
+          </div>
+        )}
+      </Modal>
+ </div>
   );
 };
 
