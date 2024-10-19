79001893e1fe4b38ba4b16c7180a0ee6c5583c4b
rina plata <rinamarcelaplata@gmail.com>
2024-10-19 01:49:32 -0500

    feat: menu de movil

--- atuuja-mov/src/navigation/AppNavigator.js
+++ atuuja-mov/src/navigation/AppNavigator.js
@@ -28,12 +28,14 @@ const TabNavigator = () => (
     screenOptions={{
       tabBarStyle: { backgroundColor: colors.principal }, // Color de la barra inferior
       tabBarActiveTintColor: colors.luminous, // Íconos activos
-      tabBarInactiveTintColor: colors.variante3, // Íconos inactivos
+      tabBarInactiveTintColor: colors.variante3,// Íconos inactivos
+      headerShown: false, 
     }}>
     <Tab.Screen name="Home" component={HomeScreen} options={{
         tabBarIcon: ({ color, size }) => (
           <MaterialIcons name="home" color={color} size={size} />
         ),
+        headerShown: false,
       }} />
     <Tab.Screen name="Learn" component={LearnScreen} options={{
         tabBarIcon: ({ color, size }) => (
--- atuuja-mov/src/screens/HomeScreen.js
+++ atuuja-mov/src/screens/HomeScreen.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import colors from '../constants/colors';
-import { useAuth } from '../context/AuthContext'; 
-
 
 const HomeScreen = () => {
-  const { user } = useAuth();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.subtitle}>Bienvenido de vuelta,</Text>
-      <Text style={styles.title}>{user?.displayName|| 'Usuario'}</Text> 
-    </View>   
-    
+      <View style={styles.topMenu}>
+        <View style={styles.pointsContainer}>
+          <Image
+            source={require('../../assets/icons/images/logoBanner.png')}
+            style={styles.pointsIcon}
+          />
+          <Text style={styles.pointsText}>120</Text>
+        </View>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require('../../assets/icons/images/isologoBanner.png')}
+            style={styles.logo}
+          />
+        </View>
+        <View style={styles.progressContainer}>
+        </View>
+      </View>
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.subtitle}>Bienvenido de vuelta, Rina</Text>
+    </View>
+    </View>
   );
 };
 
@@ -20,8 +34,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.principal,
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  topMenu: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    marginBottom: 20,
+  },
+  pointsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  pointsIcon: {
+    width: 24,
+    height: 24,
+    marginRight: 5,
+  },
+  pointsText: {
+    color: '#FFF',
+    fontSize: 16,
+  },
+  logoContainer: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+  },
+  progressContainer: {
+    width: 50,
+    height: 50,
+  },
+  welcomeContainer: {
+    marginBottom: 20,
   },
   title: {
     color: colors.luminous,
